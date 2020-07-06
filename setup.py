f369e5a55b68b0c0ff3583ea9a607c79ca1d3553
m00ga <sunrineme@gmail.com>
2020-07-06 22:39:46 +0300

    change name and setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 import config
 
 setup(
-    name='typecheck-m00ga',
+    name='typeguarder',
     version=config.__version__,
     packages=['typecheck'],
     author=config.__author__,
     description='Useful module for runtime type checking',
-    setup_requires=['wheel']
+    setup_requires=['wheel'],
+    url='https://github.com/m00ga/typecheck',
 )
